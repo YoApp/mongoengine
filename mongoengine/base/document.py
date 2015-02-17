d3451a8ca85f3146605c79c4dff13342a2dc1bdb
Michael Chase <mikec@justyo.co>
2015-02-17 13:36:09 -0800

    Only mutate the current Object definition in __setstate__

--- mongoengine/base/document.py
+++ mongoengine/base/document.py
@@ -175,7 +175,16 @@ class BaseDocument(object):
             if k in data:
                 setattr(self, k, data[k])
         if '_fields_ordered' in data:
-            setattr(type(self), '_fields_ordered', data['_fields_ordered'])
+            _super_fields_ordered = type(self)._fields_ordered
+            _fields_ordered = data['_fields_ordered']
+            # Add new fields to the existing Document instance
+            for field in _super_fields_ordered:
+                if field not in _fields_ordered:
+                    _fields_ordered += (field, )
+
+            setattr(self, '_fields_ordered', _fields_ordered)
+
+
         dynamic_fields = data.get('_dynamic_fields') or SON()
         for k in dynamic_fields.keys():
             setattr(self, k, data["_data"].get(k))
